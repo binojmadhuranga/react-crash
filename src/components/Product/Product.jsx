7d1be8422a4e4f289091649fd9f5b1d19a0878ec
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-14 17:38:55 +0530

    changers

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Product.css'
 
+
+ 
+
+
 function Product(prop) {
   return (
     <div className='product'>
         <h1>{prop.tittle}</h1>
         <p>{prop.description}</p>
-
-
-
+        <p>{prop.stock}</p>
+        <button>Add to Cart</button>
+        <button>Buy Now</button>
 
     </div>
   )
